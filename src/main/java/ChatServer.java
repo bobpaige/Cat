ca4387d3d9c9510bef5391073aae3814dff35e3c
bobpaige <bobpaige@github.com>
2016-02-06 14:18:51 -0500

    Display remote address

--- src/main/java/ChatServer.java
+++ src/main/java/ChatServer.java
@@ -1,3 +1,4 @@
+
 /* Java source code for Chat Server
  * Original code written by: Bob Paige
  * Additional editing by: Jon Paige
@@ -25,11 +26,14 @@ public class ChatServer {
         int portNumber;
         try {
             portNumber = Integer.parseInt(System.getProperty("port", "1337"));
-            //System.out.println("Enter port number for server: ");
-            //portNumber = scan.nextInt();
+            // System.out.println("Enter port number for server: ");
+            // portNumber = scan.nextInt();
             System.out.println("Waiting for connection");
             ServerSocket socket = new ServerSocket(portNumber);
             Socket connection = socket.accept();
+
+            String remoteName = connection.getRemoteSocketAddress().toString();
+
             System.out.println("Accepted connection");
             InputStream input = connection.getInputStream();
             OutputStream output = connection.getOutputStream();
@@ -37,28 +41,28 @@ public class ChatServer {
             BufferedReader brin = new BufferedReader(rin);
             Writer wout = new OutputStreamWriter(output);
 
-            while(true){
+            while (true) {
                 s = brin.readLine();
-                //System.out.println("Received '" + s + "' from client");
-                if(s.startsWith("/")){
-                    //System.out.println("Command input on server");
-                    if(s.contains("shutdown")){
+                // System.out.println("Received '" + s + "' from client");
+                if (s.startsWith("/")) {
+                    // System.out.println("Command input on server");
+                    if (s.contains("shutdown")) {
                         System.out.println("Server shutdown by: " + "{Address of client}");
                         break;
-                    } else if(s.contains("help")){
+                    } else if (s.contains("help")) {
                         System.out.println("Help command called by: " + "{Address of client}");
-                    } else if(s.contains("info")){
+                    } else if (s.contains("info")) {
                         System.out.println("Info command called by: " + "{Address of client}");
                     } else {
                         System.out.println("Unknown command called by: " + "{Address of client}");
                     }
                 } else {
-                    System.out.println("{Address of client}" + ": " + s);
-                    wout.write("{Address of client}: " + s + "\n");
+                    System.out.println(remoteName + ": " + s);
+                    wout.write(remoteName + ": " + s + "\n");
                     wout.flush();
                 }
             }
-            //socket.close();
+            // socket.close();
             System.out.println("Closing connection");
         } catch (IOException e) {
             // TODO Auto-generated catch block
